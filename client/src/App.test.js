b818625b0dbc42ee400da85c8ebf82534fbe9b9b
dunnjacoba <101910318+dunnjacoba@users.noreply.github.com>
2022-09-14 23:38:40 -0500

    [Update] Testing

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,8 +1,14 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("renders create button", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Create/i);
   expect(linkElement).toBeInTheDocument();
+});
+
+test("renders carousel", () => {
+  render(<App />);
+  const imgShown = screen.getByAltText(/Slide 1/i);
+  expect(imgShown).toBeInTheDocument();
 });
