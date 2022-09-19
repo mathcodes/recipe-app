98e29d55662feb8e3159eca1ac47919c269371b9
dunnjacoba <101910318+dunnjacoba@users.noreply.github.com>
2022-09-19 16:46:34 -0500

    [Fix] removes unused useState from App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,9 @@
-import { useState } from "react";
 import Main from "./components/Main";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter } from "react-router-dom";
 
 function App() {
-  
   return (
     <BrowserRouter>
       <Main />
