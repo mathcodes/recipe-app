8f489715f1ebf5b2cded21ad338c088eda877be6
mathcodes <jonpchristie@gmail.com>
2022-10-27 04:23:56 -0400

    styled header and used react-icons for chevron icon

--- client/src/components/StepComponents/ModalComponent.js
+++ client/src/components/StepComponents/ModalComponent.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Modal, ModalBody } from 'reactstrap';
+import { FaChevronDown } from 'react-icons/fa';
 
 function ModalComponent( {shown, setShown} ) {
   const close = ()=> setShown({title:'',contents:''})
@@ -8,9 +9,11 @@ function ModalComponent( {shown, setShown} ) {
       <div>
         <Modal isOpen={shown.title} toggle={close}>
           {/* Header still needs styled and chevron added after icon lib is decided on */}
-          <div className='modalHeader'>
-            {shown.title} 
-            <span style={{cursor:'pointer'}} onClick={close}>X</span>
+          <div className='modalHeader' >
+            <h3 className="modal-title text-center">{shown.title} </h3>
+            <span className="position-absolute top-0 end-0" style={{cursor:'pointer'}} onClick={close}>
+            <FaChevronDown />
+            </span>
           </div>
           <ModalBody>  
             {shown.contents}
